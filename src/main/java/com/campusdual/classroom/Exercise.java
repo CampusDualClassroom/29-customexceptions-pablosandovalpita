2202cda79fce21d90c44ca3066e4fbc203a5018b
pablosandovalpita <pablo.sandoval@campusdual.com>
2025-11-13 09:05:04 +0100

    ejercicio 29

--- src/main/java/com/campusdual/classroom/Exercise.java
+++ src/main/java/com/campusdual/classroom/Exercise.java
@@ -1,12 +1,26 @@
 package com.campusdual.classroom;
 
+
+class DivisionByZeroException extends Exception {
+    public DivisionByZeroException(String message) {
+        super(message);
+    }
+}
+
 public class Exercise {
 
-    public static int divisionWithCustomException(int dividend, int divisor){
-
+    public static int divisionWithCustomException(int dividend, int divisor) throws DivisionByZeroException {
+        if (divisor == 0) {
+            throw new DivisionByZeroException("No se puede dividir entre cero");
+        }
+        return dividend / divisor;
     }
 
     public static void main(String[] args) {
-
+        try {
+            System.out.println(divisionWithCustomException(10, 0));
+        } catch (DivisionByZeroException e) {
+            System.out.println("Se ha capturado la excepción personalizada: " + e.getMessage());
+        }
     }
 }
